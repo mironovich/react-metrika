bd170a8a0f237fc7cb26bac4532d400ee2008c9b
Всеволод Миронович <mironovich@ketchmac.local>
2018-10-11 10:24:27 +0300

    Updates

---
+++ precache-manifest.23ac4d8c619f314a29e87f734012c384.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3505dc38ced0e28e79",
+    "url": "/react-metrika/static/js/runtime~main.3e3505dc.js"
+  },
+  {
+    "revision": "8a66ba1765ab465f2724",
+    "url": "/react-metrika/static/js/main.8a66ba17.chunk.js"
+  },
+  {
+    "revision": "97b6108facfc238b0af6",
+    "url": "/react-metrika/static/js/1.97b6108f.chunk.js"
+  },
+  {
+    "revision": "8a66ba1765ab465f2724",
+    "url": "/react-metrika/static/css/main.4f498715.chunk.css"
+  },
+  {
+    "revision": "5925e832b48f5ada67d6ae0963ef815b",
+    "url": "/react-metrika/index.html"
+  }
+];
